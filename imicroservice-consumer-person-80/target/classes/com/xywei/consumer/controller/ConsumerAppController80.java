501c99821e77e8c053e5b60d3c00ffa1dee9f3a4
nobody <insoul_net@qq.com>
2019-03-31 13:28:52 +0800

    完成consumer，但是postForObject请求参数丢失

--- imicroservice-consumer-person-80/target/classes/com/xywei/consumer/controller/ConsumerAppController80.java
+++ imicroservice-consumer-person-80/target/classes/com/xywei/consumer/controller/ConsumerAppController80.java
@@ -1,7 +1,11 @@
 package com.xywei.consumer.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,15 +14,28 @@ import com.xywei.domain.Person;
 @RestController
 public class ConsumerAppController80 {
 
-	private static final String url = "localhost:8001";
-	
+	private static final String URL_PROVIDER = "http://127.0.0.1:8001";
+
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@RequestMapping(value="consumer/person/add")
-	public boolean addPersion(Person person){
-		
-		return true;
+	@RequestMapping(value = "consumer/person/add", method = RequestMethod.GET)
+	public boolean addPerson(Person person) {
+//		Map<String, String> mapPerson = new HashMap<String, String>();
+//		mapPerson.put("personName", person.getPersonName());
+		return restTemplate.getForObject(URL_PROVIDER + "/add", Boolean.class, person);
+//		return restTemplate.postForObject(URL_PROVIDER + "/add", person, Boolean.class, person.getPersonName());
+	}
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping("/consumer/findAll")
+	public List<Person> findAllPersons() {
+		return restTemplate.getForObject(URL_PROVIDER + "/findAll", List.class);
+	}
+
+	@RequestMapping("consumer/getPerson/{id}")
+	public Person findPersonByID(@PathVariable Long id) {
+		return restTemplate.getForObject(URL_PROVIDER + "/getPerson/" + id, Person.class);
 	}
 
 }
