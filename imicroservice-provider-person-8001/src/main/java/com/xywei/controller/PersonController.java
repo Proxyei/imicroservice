bb0673a247c6390b35082eebebf3b7e7133ec00a
nobody <insoul_net@qq.com>
2019-03-31 17:43:56 +0800

    微服务被发现

--- imicroservice-provider-person-8001/src/main/java/com/xywei/controller/PersonController.java
+++ imicroservice-provider-person-8001/src/main/java/com/xywei/controller/PersonController.java
@@ -3,6 +3,8 @@ package com.xywei.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +18,10 @@ public class PersonController {
 
 	@Autowired
 	private PersonService personService;
+
+	// 服务发现
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
 	@RequestMapping(value = "add", method = RequestMethod.GET)
 	public boolean addPerson(Person person) {
@@ -31,4 +37,21 @@ public class PersonController {
 	public List<Person> findAllPerson() {
 		return personService.findAllPerson();
 	}
+
+	@RequestMapping(value = "discoveryService", method = RequestMethod.GET)
+	public Object discoveryService() {
+		// 微服务总数,与下面的instance是一个意思
+		List<String> services = discoveryClient.getServices();
+		System.out.println("微服务总数有：" + services);
+		// 微服务中的示例：
+		List<ServiceInstance> serviceInstances = discoveryClient.getInstances("imicroservice-person");
+		for (ServiceInstance instance : serviceInstances) {
+			System.out.println("service id = " + instance.getServiceId() + " service host = " + instance.getHost()
+					+ " service post = " + instance.getPort() + " URI = " + instance.getUri());
+
+		}
+
+		return this.discoveryClient;
+	}
+
 }
