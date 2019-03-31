fdb8c5da1f23c3fad073b1d1387d42a6eb1b3d02
nobody <insoul_net@qq.com>
2019-03-31 15:14:53 +0800

    注册服务类作为eureka客户的

--- imicroservice-provider-person-8001/target/classes/com/xywei/PersonApp8001.java
+++ imicroservice-provider-person-8001/target/classes/com/xywei/PersonApp8001.java
@@ -2,8 +2,10 @@ package com.xywei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PersonApp8001 {
 
 	public static void main(String[] args) {
